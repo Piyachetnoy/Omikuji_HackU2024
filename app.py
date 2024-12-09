79a906d91cb3ea5038639b39ee8700f845db45bf
Piyachetnoy <piyachet2310noy@gmail.com>
2024-12-09 18:42:31 +0900

    showcase page

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify,render_template
+import random
 import os
 
 app = Flask(__name__)
@@ -10,10 +11,26 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 # Set the upload folder in the app configuration
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+def get_random_image():
+    upload_folder = os.path.join(app.root_path, 'static', 'upload') # Get the list of files in the 'upload' folder
+    image_files = os.listdir(upload_folder) # Choose a random image from the list
+    return random.choice(image_files)
+
 # Set the template of html file (put htmls in templates folder)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/showcase')
+def showcase():
+    random_image = get_random_image()
+    return render_template('showcase.html', random_image=random_image) # Return the 'showcase.html' template and pass the random image filename
+
+@app.route('/random_image')
+def random_image():
+    random_image = get_random_image()
+    return jsonify({'random_image': random_image}) # Return the filename of the random image
+
 
 @app.route('/process-upload', methods=['POST'])
 def process_upload():
@@ -32,6 +49,7 @@ def process_upload():
 
     # return jsonify({"success": True, "filePath": file_path}), 200
     return render_template("result.html", name = file_path)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
